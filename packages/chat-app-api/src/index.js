3f2bac39138fa157743b23952214a44e697b50f3
Valentina Zea <mavalezea@gmail.com>
2021-09-13 19:49:58 -0500

    update: Config Lerna script for running dev environment

--- packages/chat-app-api/src/index.js
+++ packages/chat-app-api/src/index.js
@@ -1,1 +1,16 @@
-console.log('Hola Mundooooo');
+const express = require('express');
+
+const port = process.env.PORT || 8081;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('SOY EL BACKEND');
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log(`Listening on port ${port}`);
+  }
+});
